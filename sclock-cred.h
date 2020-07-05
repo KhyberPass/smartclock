7bb8dc6a7208461093f542e0a862c933e9d401f4
KhyberPass <nick.schicht@gmail.com>
2020-07-05 22:24:45 +1000

    Fix up ignored file

---
+++ sclock-cred.h
@@ -0,0 +1,11 @@
+#ifndef _SCLOCK_CRED_H_
+#define _SCLOCK_CRED_H_
+
+// This file exists just so I do not have to check the real passwords into Github
+
+#define WIFI_SSID "SSID here"
+#define WIFI_PASS "password here"
+#define WIFI_SSID_ALT1 "SSID here"
+#define WIFI_PASS_ALT1 "password here"
+
+#endif // _SCLOCK_CRED_H_
