a9392be100970910608b3bf4484fa0b4ff57c17f
KhyberPass <nick.schicht@gmail.com>
2020-07-06 09:45:37 +1000

    Revert to working time file

--- sclock-time.ino
+++ sclock-time.ino
@@ -1,128 +1,60 @@
-#include <TimeLib.h>
-#include <Timezone.h>
-#include <WiFiUdp.h>
+#include <ezTime.h>
 
-// NTP Servers:
-IPAddress timeServer(162, 159, 200, 123); //pool.ntp.org also 103.126.53.123
-
-//IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov
-// IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov
-// IPAddress timeServer(132, 163, 4, 103); // time-c.timefreq.bldrdoc.gov
-
-const int timeZone = 0;
-//const int timeZone = 1;     // Central European Time
-//const int timeZone = -5;  // Eastern Standard Time (USA)
-//const int timeZone = -4;  // Eastern Daylight Time (USA)
-//const int timeZone = -8;  // Pacific Standard Time (USA)
-//const int timeZone = -7;  // Pacific Daylight Time (USA)
-
-WiFiUDP Udp;
-unsigned int localPort = 8888;  // local port to listen for UDP packets
-
-// Australia Eastern Time Zone (Sydney, Melbourne)
-TimeChangeRule aEDT = {"AEDT", First, Sun, Oct, 2, 660};    // UTC + 11 hours
-TimeChangeRule aEST = {"AEST", First, Sun, Apr, 3, 600};    // UTC + 10 hours
-Timezone ausET(aEDT, aEST);
+int previousMinute = 0;
+bool timeChanged = true;
+Timezone myTime;
 
 void sclockTimeSetup(void)
 {
-  Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(Udp.localPort());
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
-
-  //setTime(12, 00, 00, 7, 01, 2020);
-
-  delay(1000);
-  Serial.println(__TIME__);
+  bool status;
   
-  if (timeStatus()!= timeSet) 
-     Serial.println("Unable to sync with the RTC");
-  else
-     Serial.println("RTC has set the system time");      
+  setDebug(INFO);
+  
+  status = waitForSync();
+  
+  if (status == false) {
+    Serial.println("Time sync failed");
+  }
+  
+  myTime.setLocation("Australia/Sydney");
+  Serial.println("Time: " + myTime.dateTime());
+
+  previousMinute = minute();
 }
 
 void sclockTimeLoop(void)
 {
-//  now();
+  // Call the events system, this will periodically
+  // do a NTP update
+  events();
 
-#if 0
-  time_t utc = now();
-  time_t local = ausET.toLocal(utc);
-  Serial.print(hour(local));
-  Serial.print(":");
-  Serial.print(minute(local));
-  Serial.print(":");
-  Serial.println(second(local));
-#endif
-  
-#if 0  
-  Serial.print(hour());
-  Serial.print(":");
-  Serial.print(minute());
-  Serial.print(":");
-  Serial.println(second());
-#endif
-}
-
-
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  sendNTPpacket(timeServer);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      time_t t = secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-  time_t local = ausET.toLocal(t);
-  Serial.print(hour(local));
-  Serial.print(":");
-  Serial.print(minute(local));
-  Serial.print(":");
-  Serial.println(second(local));
-      
-      return t;
-    }
+  // Check if the time minutes has changed, if it has
+  // then flag that the time has changed
+  if (previousMinute != minute())
+  {
+    //Serial.println("Time changed " + myTime.dateTime());
+    timeChanged = true;
+    previousMinute = minute();
   }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
 }
 
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:                 
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
+bool sclockTimeHasChanged() {
+  if (timeChanged) 
+  {
+    timeChanged = false;
+    return true;
+  }
+  return false;
+}
+
+String sclockTimeGet() {
+  return myTime.dateTime("H:i");
+}
+
+String sclockTimeHoursGet() {
+  return myTime.dateTime("H");
+}
+
+String sclockTimeMinutesGet() {
+  return myTime.dateTime("i");
 }
